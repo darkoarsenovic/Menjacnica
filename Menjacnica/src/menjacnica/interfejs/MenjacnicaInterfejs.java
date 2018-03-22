9ee7fbe70d4b2c08d9c85c3f96def99e1c2f2aad
darkoarsenovic <daekoars@yahoo.com>
2018-03-22 13:38:18 +0100

    Definisan javni interfejs MenjacnicaInterfejs
    u paketu menjacnica.intefejs sa javnim metodama dodajKurs, obrisiKurs i
    pronadjiKurs.

---
+++ Menjacnica/src/menjacnica/interfejs/MenjacnicaInterfejs.java
@@ -0,0 +1,10 @@
+package menjacnica.interfejs;
+
+public interface MenjacnicaInterfejs {
+
+	public void dodajKurs(double prodajni, double srednji, double kupovni);
+
+	public void obrisiKurs();
+
+	public double pronadjiKurs();
+}
