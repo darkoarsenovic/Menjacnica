a90712c66d86e097f47d3cfbc6448be693d89563
darkoarsenovic <daekoars@yahoo.com>
2018-03-22 13:58:12 +0100

    Kreirana javna klasa Menjacnica koja implementira MenjacnicaInterjefs
    Implementirane metode koje je potrebno definisati.

---
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,25 @@
+package menjacnica;
+
+import menjacnica.interfejs.MenjacnicaInterfejs;
+
+public class Menjacnica implements MenjacnicaInterfejs {
+
+	@Override
+	public void dodajKurs(double prodajni, double srednji, double kupovni) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void obrisiKurs() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public double pronadjiKurs() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
